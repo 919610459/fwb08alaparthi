c851d02ac765005ed5b18b8ec490b311e710fd5a
919610459 <S545508@NWMISSOURI.EDU>
2021-11-03 22:08:10 -0500

    added and updated selector file

--- routes/selector.js
+++ routes/selector.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('dog', { title: 'search results for dog' });
+  res.render('selector', { title: 'selector' });
 });
 
 module.exports = router;
